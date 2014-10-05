ec82551551667fda8a2f6ec577009736f120ef43
Matt Brennan <mattyb1000@gmail.com>
2014-10-05 09:55:08 +0100

    more tests

--- test.js
+++ test.js
@@ -16,5 +16,32 @@ describe('async-flat-map', function() {
 			done(e);
 		});
 	});
+
+	it('should pass first error along', function(done) {
+		var e = {};
+		asyncFlatMap(function(x, cb) {
+			if(x === 2) {
+				cb(e);
+			} else cb(null, [x, x*2]);
+		}, [1,2,3], function(err) {
+			expect(e).to.be(e);
+			done();
+		});
+	});
+
+	it('should run in parallel', function(done) {
+		var that = this;
+
+		asyncFlatMap(function(x, cb) {
+			that.timer.setTimeout(function() {
+				cb(null, [x, x * 2]);
+			}, 100);
+		}, [1,2,3], function(e, ys) {
+			expect(ys).to.eql([1,2,2,4,3,6]);
+			done(e);
+		});
+
+		this.timer.wind(100); // would take 300ms in series
+	});
 });
 
